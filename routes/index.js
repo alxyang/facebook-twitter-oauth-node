6498671722a68cf60822ea00d5f5078dc735d0f6
Alex Yang <alyx.yang@gmail.com>
2014-04-17 00:05:28 -0700

    pass data to handlebars

--- routes/index.js
+++ routes/index.js
@@ -6,21 +6,32 @@ exports.view = function(req, res) {
  }
 
 exports.fbpage = function(req, res) {
-
+    var tempdata;
     //test api call facebook
     app.graph.get("/me?fields=feed", function(err, res) {
       console.log(JSON.stringify(res,  null, '\t')); // get my information as json
+      tempdata = JSON.stringify(res,  null, '\t');
     });
-    res.render('fbpage');
+    var data = {rdata : tempdata};
+    res.render('fbpage', data);
 }
 
 exports.twitpage = function(req, res) {
-
-   
     //  get user twitter feed test
-    
     app.T.get('statuses/user_timeline', { count: 2 }, function(err, reply) {
-      console.log(reply);
+
+        var testarray = [];
+        reply.map(function(item){
+            var tempJSON = {};
+            tempJSON.text = item.text;
+            testarray.push(tempJSON);
+        });
+
+        // console.log(testarray[0]);
+        // console.log(testarray[1]);
+
+        var data = {rdata: "herp", tdata: "derp", posts: testarray};
+        res.render('twitpage', data);
     })
-    res.render('twitpage');
 }
+
