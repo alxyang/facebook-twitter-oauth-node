c4686c9a96d82146acb2e277f77a7a90ecc2ca37
Calvin Chan <calvinwchan@me.com>
2014-04-17 17:40:55 -0700

    timestamp and minor css changes

--- routes/index.js
+++ routes/index.js
@@ -49,16 +49,36 @@ exports.twitpage = function(req, res) {
     //  get user twitter feed test
     app.T.get('statuses/user_timeline', { count: 20 }, function(err, reply) {
 
-        var testarray = [];
+        var textArr = [];
         reply.map(function(item){
-            var textArr = {};
-            textArr.text = item.text;
-            testarray.push(textArr);
+            var textJSON = {};
+            textJSON.text = item.text;
+            textJSON.time = parseTwitterDate(item.created_at);
+            textArr.push(textJSON);
         });
 
-        posts = testarray;
+        posts = textArr;
         complete();
     });
+
+    // used to transform twitter's timestamp into something more readable
+    function parseTwitterDate(tdate) {
+		var system_date = new Date(Date.parse(tdate));
+		var user_date = new Date();
+		var diff = Math.floor((user_date - system_date) / 1000);
+		if (diff <= 1) {return "just now";}
+		if (diff < 20) {return diff + " seconds ago";}
+		if (diff < 40) {return "half a minute ago";}
+		if (diff < 60) {return "less than a minute ago";}
+		if (diff <= 90) {return "one minute ago";}
+		if (diff <= 3540) {return Math.round(diff / 60) + " minutes ago";}
+		if (diff <= 5400) {return "1 hour ago";}
+		if (diff <= 86400) {return Math.round(diff / 3600) + " hours ago";}
+		if (diff <= 129600) {return "1 day ago";}
+		if (diff < 604800) {return Math.round(diff / 86400) + " days ago";}
+		if (diff <= 777600) {return "1 week ago";}
+		return "on " + system_date;
+	}
 
     // check to make sure that async calls are made before final render
     function complete() {
